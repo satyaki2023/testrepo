661f8aded2c11bcdce902c9468652aa908cf2a5a
satyaki2023 <138913374+satyaki2023@users.noreply.github.com>
2023-07-08 02:15:11 +0530

    Add files via upload

---
+++ first_python_code.py
@@ -0,0 +1,13 @@
+import time
+x, y, z = 3, 4, 5
+print(x)
+print(type(x))
+time.sleep(2)
+print(y)
+print(type(y))
+time.sleep(2)
+print(z)
+print(type(z))
+time.sleep(2)
+print(x+y+z)
+print("End of program")
