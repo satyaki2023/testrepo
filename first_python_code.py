010514c0ff5e29277d987ae3490e8e920ece4bee
Yamraj Chaudhary <yamrajpanwar333@gmail.com>
2023-07-11 19:44:54 +0530

    modified first_python_code.py and added new file browser-support.txt

--- first_python_code.py
+++ first_python_code.py
@@ -2,12 +2,12 @@ import time
 x, y, z = 3, 4, 5
 print(x)
 print(type(x))
-time.sleep(2)
+time.sleep(5) #updated sleep time from 2 to 5 seconds
 print(y)
 print(type(y))
-time.sleep(2)
+time.sleep(5) #updated sleep time from 2 to 5 seconds
 print(z)
 print(type(z))
-time.sleep(2)
+time.sleep(5) #updated sleep time from 2 to 5 seconds
 print(x+y+z)
 print("End of program")
