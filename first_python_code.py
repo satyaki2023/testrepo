c33a27d1cf1ce5a3ada1cb717bd06d312020565b
Satyaki Baidya <satyaki.baidyaone2023@gmail.com>
2023-07-12 14:48:42 +0530

    updated the first_python_code.py file

--- first_python_code.py
+++ first_python_code.py
@@ -1,13 +1,14 @@
 import time
 x, y, z = 3, 4, 5
+time.sleep(3) # added a 3 seconds sleep time before first print
 print(x)
 print(type(x))
-time.sleep(5) #updated sleep time from 2 to 5 seconds
+time.sleep(5) # updated sleep time from 2 to 5 seconds
 print(y)
 print(type(y))
-time.sleep(5) #updated sleep time from 2 to 5 seconds
+time.sleep(5) # updated sleep time from 2 to 5 seconds
 print(z)
 print(type(z))
-time.sleep(5) #updated sleep time from 2 to 5 seconds
+time.sleep(5) # updated sleep time from 2 to 5 seconds
 print(x+y+z)
 print("End of program")
